9cde94661fb95b6ce951f071dea02603d215e815
Nishant Kumar <nishants878@gmail.com>
2021-06-16 18:32:23 +0530

    admin app sidbar, topbar

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import './chart.css';
+import {
+    LineChart,
+    Line,
+    XAxis,
+    CartesianGrid,
+    Tooltip,
+    ResponsiveContainer,
+  } from "recharts";
+export default function Chart() {
+    const data = [
+        {
+          name: 'Jan',
+          "Active User": 4000,
+          
+        },
+        {
+            name: 'Feb',
+            "Active User": 5000,
+            
+          },
+          {
+            name: 'Mar',
+            "Active User": 1000,
+            
+          },
+          {
+            name: 'Apr',
+            "Active User": 8000,
+            
+          },
+          {
+            name: 'May',
+            "Active User": 4000,
+            
+          },
+          {
+            name: 'Jun',
+            "Active User": 3000,
+            
+          },
+          {
+            name: 'Jul',
+            "Active User": 9000,
+            
+          },
+          {
+            name: 'Aug',
+            "Active User": 8000,
+            
+          },
+          {
+            name: 'Sept',
+            "Active User": 5000,
+            
+          },
+          {
+            name: 'Oct',
+            "Active User": 8000,
+            
+          },
+          {
+            name: 'Nov',
+            "Active User": 1000,
+            
+          },
+          {
+            name: 'Dec',
+            "Active User": 2000,
+            
+          },
+       
+      ];
+    return (
+        <div className="chart">
+            <h3 className="chartTitle">Sales Analytics</h3>
+            <ResponsiveContainer width="100%" aspect={4 / 1}>
+                <LineChart data={data}>
+                    <XAxis dataKey="name" stroke="#5550bd"/>
+                    <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
+                    <Tooltip/>
+                    <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
