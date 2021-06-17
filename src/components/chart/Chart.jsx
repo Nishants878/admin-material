c862e70f5c6fc9704b962d3de7717466f5395527
Nishant Kumar <nishants878@gmail.com>
2021-06-17 11:25:09 +0530

    ui complted

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -8,80 +8,18 @@ import {
     Tooltip,
     ResponsiveContainer,
   } from "recharts";
-export default function Chart() {
-    const data = [
-        {
-          name: 'Jan',
-          "Active User": 4000,
-          
-        },
-        {
-            name: 'Feb',
-            "Active User": 5000,
-            
-          },
-          {
-            name: 'Mar',
-            "Active User": 1000,
-            
-          },
-          {
-            name: 'Apr',
-            "Active User": 8000,
-            
-          },
-          {
-            name: 'May',
-            "Active User": 4000,
-            
-          },
-          {
-            name: 'Jun',
-            "Active User": 3000,
-            
-          },
-          {
-            name: 'Jul',
-            "Active User": 9000,
-            
-          },
-          {
-            name: 'Aug',
-            "Active User": 8000,
-            
-          },
-          {
-            name: 'Sept',
-            "Active User": 5000,
-            
-          },
-          {
-            name: 'Oct',
-            "Active User": 8000,
-            
-          },
-          {
-            name: 'Nov',
-            "Active User": 1000,
-            
-          },
-          {
-            name: 'Dec',
-            "Active User": 2000,
-            
-          },
-       
-      ];
+export default function Chart({ title, data, dataKey, grid }) {
+    
     return (
         <div className="chart">
-            <h3 className="chartTitle">Sales Analytics</h3>
+            <h3 className="chartTitle">{title}</h3>
             <ResponsiveContainer width="100%" aspect={4 / 1}>
-                <LineChart data={data}>
-                    <XAxis dataKey="name" stroke="#5550bd"/>
-                    <Line type="monotone" dataKey="Active User" stroke="#5550bd" />
-                    <Tooltip/>
-                    <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />
-                </LineChart>
+            <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
             </ResponsiveContainer>
         </div>
     )
